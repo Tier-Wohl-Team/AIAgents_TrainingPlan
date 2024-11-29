f1b690b81191ff5f2e3a95bc239ea1ef7af08573
Joerg Schultz <joergsky@online.de>
2024-11-29 11:19:16 +0100

    integrated Behavior Research Team into the Training Plan Team

--- TrainingPlan_Team/team.py
+++ TrainingPlan_Team/team.py
@@ -1,15 +1,16 @@
 # %% settings
 import sys
 import os
+
 sys.path.append(os.path.abspath("./TrainingPlan_Team"))  # Adjust the path as needed
 
 from langgraph.constants import START, END
 from langgraph.graph import StateGraph
 
-
 from teams.SpecialistWelfareTeam import SpecialistWelfareTeam
-from states.state_types import TeamState, BehaviorState
-from agents.OutlineWriter import OutlineWriter
+from teams.BehaviorResearchTeam import BehaviorResearchTeam
+
+from states.state_types import TeamState
 from agents.SpecialistsTeamLeader import SpecialistsTeamLeader
 from agents.DistanceDurationSpecialist import DistanceDurationSpecialist
 from agents.CueSpecialist import CueSpecialist
@@ -17,6 +18,8 @@ from agents.DistractionSpecialist import DistractionSpecialist
 from agents.Generalist import Generalist
 
 # %% Build mini teams
+behavior_research_team = BehaviorResearchTeam(name="Behavior Research Team")
+
 distance_duration_team = SpecialistWelfareTeam(name="Distance Duration Team", specialist_agent=DistanceDurationSpecialist)
 cue_team = SpecialistWelfareTeam(name="Cue Specialist Team", specialist_agent=CueSpecialist)
 distraction_team = SpecialistWelfareTeam(name="Distraction Specialist Team", specialist_agent=DistractionSpecialist)
@@ -35,14 +38,14 @@ def collector(state: TeamState):
 
 # %% build the team graph
 team_graph_builder = StateGraph(TeamState)
-team_graph_builder.add_node(OutlineWriter.NAME, OutlineWriter.action)
+team_graph_builder.add_node(behavior_research_team.name, behavior_research_team.graph)
 team_graph_builder.add_node(distance_duration_team.name, distance_duration_team.graph)
 team_graph_builder.add_node(cue_team.name, cue_team.graph)
 team_graph_builder.add_node(distraction_team.name, distraction_team.graph)
 team_graph_builder.add_node(generalist_team.name, generalist_team.graph)
 team_graph_builder.add_node("Collector", collector)
-team_graph_builder.add_edge(START, OutlineWriter.NAME)
-team_graph_builder.add_conditional_edges(OutlineWriter.NAME, SpecialistsTeamLeader.action,
+team_graph_builder.add_edge(START, behavior_research_team.name)
+team_graph_builder.add_conditional_edges(behavior_research_team.name, SpecialistsTeamLeader.action,
                                     [distance_duration_team.name, cue_team.name, distraction_team.name, generalist_team.name])
 team_graph_builder.add_edge(distance_duration_team.name, "Collector")
 team_graph_builder.add_edge(cue_team.name, "Collector")
@@ -52,8 +55,9 @@ team_graph_builder.add_edge("Collector", END)
 # %% compile the team graph
 team = team_graph_builder.compile()
 
-# %% test graph
-# question = "When retrieving,, my dog drops the ball 2 meters in front of me. I want him to deliver to hand."
+# # %% test graph
+# question = "I want to teach my dog laser directionals"
 # for s in team.stream({"question": question}):
 #   print(s)
+#
 
